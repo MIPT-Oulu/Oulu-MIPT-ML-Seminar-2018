63b1d9b04623a888e339b9c21c2c9c6851a118a5
lext <aleksei.tiulpin@protonmail.ch>
2018-03-15 20:04:21 +0000

    modified setup

--- Tutorial_2/set_up.sh
+++ Tutorial_2/set_up.sh
@@ -1,9 +1,5 @@
-# Loading the modules
-module load python-env/3.5.3-ml
-
-# Installing the requirements
-pip3.5 install -r requirements.txt --user
-export PATH=/homeappl/home/atiulpin/.local/bin/:$PATH
+pip install kaggle
+conda install -c bioconda p7zip
 
 # Setting things up
 mkdir $HOME/.kaggle/
@@ -13,5 +9,8 @@ echo "{\"username\":\"$1\",\"key\":\"$2\"}" > $HOME/.kaggle/kaggle.json
 kaggle competitions download -c invasive-species-monitoring --force
 mkdir data
 mv $HOME/.kaggle/competitions/invasive-species-monitoring/* data/
-# To store the logs
-mkdir logs
+
+cd data/
+7z x train.csv.zip
+unzip train_labels.csv.zip
+7z x sample_submission.csv.zip
